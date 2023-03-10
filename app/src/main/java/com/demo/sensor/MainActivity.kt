b505dafdfadd28a42d8374c3099dd0c43edea686
Sahej Singh Chandok <yash@Sahejs-MacBook-Pro.local>
2023-03-10 15:04:17 -0500

    current date and time screen

--- app/src/main/java/com/demo/sensor/MainActivity.kt
+++ app/src/main/java/com/demo/sensor/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.demo.sensor
-
+import android.os.Build
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
+import android.widget.TextView
+import java.text.SimpleDateFormat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView: TextView = findViewById(R.id.date1)
+        val simpleDateFormat = SimpleDateFormat("yyyy.MM.dd G 'at' HH:mm:ss z")
+        val currentDateAndTime: String = simpleDateFormat.format(Date())
+        textView.text = currentDateAndTime
+
+        }
     }
-}
