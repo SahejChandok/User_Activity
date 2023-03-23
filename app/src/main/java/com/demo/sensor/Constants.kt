d2f88317c9cab93d5f9eaff52a22ffc404acb544
Divyanshu Verma <divyanshu.verma1993@gmail.com>
2023-03-23 11:32:31 -0400

    Adds motion sensor with majority classifier

---
+++ app/src/main/java/com/demo/sensor/Constants.kt
@@ -0,0 +1,9 @@
+package com.demo.sensor
+
+object Constants {
+    const val RUNNING = "RUNNING"
+    const val STILL = "STILL"
+    const val WALKING = "WALKING"
+    const val IN_VEHICLE = "IN VEHICLE"
+    const val UNKNOWN = "UNKNOWN"
+}
